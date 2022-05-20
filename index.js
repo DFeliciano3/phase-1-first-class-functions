9b5b8888a06f8513348202baf7ce9af1db892ea7
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-05-20 01:11:07 -0400

    commit tree 5253f3405ce95e35611f7e1ed79313b14364c2ee

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+
+function returnsANamedFunction() {
+    return function newFunction() {
+    };
+}
+
+function returnsAnAnonymousFunction() { 
+    return function() {
+    };
+}
